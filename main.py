66e134fcbb4986b0ce119598a36a950c07816c1c
Matías Corba <matiasdavidcorba@gmail.com>
2026-07-14 16:48:15 +0000

    Refactorizacion finalizada

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import libros,usuarios,prestamos,menu_libros
+import libros,usuarios,prestamos,menus
 
 inventario={}
 registro_usuarios={}
 
-gestion=1
-while gestion > 0 and gestion < 5:
-    try:
-        gestion=int(input(" 1. Gestionar libros\n 2. Gestionar usuarios\n 3. Gestionar prestamos\n 4. Finalizar gestion\nQue desea hacer: "))
-        if gestion==1:
-           
-           if gestion_libro==1:
-            nombre=input("Ingrese el nombre del libro: ")
-            autor=input("Ingrese nombre del autor del libro: ")
-            año=input("Ingrese año de publicacion del libro: ")
-            genero=input("Ingrese el genero del libro: ")
-            inventario=libros.agregar_inventario(libro=nombre,autor=autor,año=año,genero=genero,inventario=inventario)
+while True:
+    gestion=menus.menu_principal()
+    
+    if gestion==1:
+        
+        menu=" 1. Añadir libro\n 2. Eliminar libro\n 3. Buscar libros\n 4. Volver al menu anteriror\nQue desea hacer: "
+        while True:
+            gestion_libro=menus.menu_libros
+            if gestion_libro==1:
+                nombre=input("Ingrese el nombre del libro: ")
+                autor=input("Ingrese nombre del autor del libro: ")
+                año=input("Ingrese año de publicacion del libro: ")
+                genero=input("Ingrese el genero del libro: ")
+                inventario=libros.agregar_inventario(libro=nombre,autor=autor,año=año,genero=genero,inventario=inventario)
                    
-        elif gestion_libro==2:
-            libro=input("Ingrese el nombre del libro que desea borrar: ")
-            libros.eliminar_libro(libro=libro,inventario=inventario)
+            elif gestion_libro==2:
+                libro=input("Ingrese el nombre del libro que desea borrar: ")
+                libros.eliminar_libro(libro=libro,inventario=inventario)
                                    
-        elif gestion_libro==3:
+            elif gestion_libro==3:
 
-            gestion_busqueda=1
-            while gestion_busqueda > 0 and gestion_busqueda < 6:
-                try:
-                    gestion_busqueda=int(input(" 1. Listar todos\n 2. Listar disponibles para prestar\n 3. Listar prestados\n 4. Buscar por filtro\n 5. Volver al menu anterior\nQue desea hacer:"))
+                while True:
+                    gestion_busqueda=menus.menu_busqueda
 
                     if gestion_busqueda==1:
                         print("Nuestros libros:")
@@ -41,11 +41,10 @@ while gestion > 0 and gestion < 5:
 
                     elif gestion_busqueda==4:
 
-                        filtro=1
                         autor=None
                         año=None
                         genero=None
-                        while filtro > 0 and filtro < 9:
+                        while True:
                                         
                             print("Filtros:")
                             if not autor:
@@ -61,122 +60,100 @@ while gestion > 0 and gestion < 5:
                             else:
                                 print("Genero:",genero)    
                                             
-                            try:
-                                filtro=int(input(" 1. Autor\n 2. Año de publicacion\n 3. Genero\n 4.Quitar filtro de autor\n 5. Quitar filtro de año\n 6.Quitar filtro de genero\n 7. Buscar\n 8. Volver\nDetermine filtro de la busqueda: "))
+                            filtro=menus.menu_filtros()
 
-                                if filtro==1:
-                                    autor=input("Nombre del autor: ")
-                                    autor=autor.lower()
+                            if filtro==1:
+                                autor=input("Nombre del autor: ")
+                                autor=autor.lower()
                                             
-                                elif filtro==2:
-                                    año=input("Año de publicacion: ")
+                            elif filtro==2:
+                                año=input("Año de publicacion: ")
 
-                                elif filtro==3:
-                                    genero=input("Genero: ")
-                                    genero=genero.lower()
+                            elif filtro==3:
+                                genero=input("Genero: ")
+                                genero=genero.lower()
                                             
-                                elif filtro==4:
-                                    if not autor:
-                                        print("No hay filtro de autor determinado")
-                                        continue
+                            elif filtro==4:
+                                if not autor:
+                                    print("No hay filtro de autor determinado")
+                                    continue
                                                 
-                                    autor=None
-                                    print("Filtro de autor eliminado")
+                                autor=None
+                                print("Filtro de autor eliminado")
 
-                                elif filtro==5:
-                                    if not año:
-                                        print("No hay filtro de año de publicación determinado")
-                                        continue
+                            elif filtro==5:
+                                if not año:
+                                    print("No hay filtro de año de publicación determinado")
+                                    continue
                                                 
-                                    año=None
-                                    print("Filtro de año de publicacion eliminado")
+                                año=None
+                                print("Filtro de año de publicacion eliminado")
 
-                                elif filtro==6:
-                                    if not genero:
-                                        print("No hay filtro de genero determinado")
-                                        continue
+                            elif filtro==6:
+                                if not genero:
+                                    print("No hay filtro de genero determinado")
+                                    continue
                                                 
-                                    genero=None
-                                    print("Filtro de genero eliminado")
+                                genero=None
+                                print("Filtro de genero eliminado")
 
-                                elif filtro==7:
-                                    break
+                            elif filtro==7:
+                                break
 
-                                elif filtro==8:
-                                    break
+                            elif filtro==8:
+                                break
                                             
-                            except ValueError:
-                                print("Por favor seleccione el numero de la accion que desea realizar")
                         if filtro==7:
                             libros.buscar_por_atributo(inventario=inventario,año=año,autor=autor,genero=genero)
                             
-                        elif gestion_busqueda==5:
+                    elif gestion_busqueda==5:
                                     break
 
-                except ValueError:
-                    print("Por favor seleccione el numero de la accion que desea realizar")
+            elif gestion_libro==4:
+                break
+                           
+    elif gestion==2:
 
-        elif gestion_libro==4:
-            break
-               
-    except ValueError:
-        print("Por favor seleccione el numero de la accion que desea realizar")
-          
+        while True:
+            gestion_usuario=menus.menu_usuarios()
                    
-        elif gestion==2:
-
-            gestion_usuario=1
-            while gestion_usuario > 0 and gestion_usuario < 5:
-               try:
-                   gestion_usuario=int(input(" 1. Añadir usuario\n 2. Eliminar usuario\n 3. Listar usuarios\n 4. Volver al menu anteriror\nQue desea hacer: "))
+            if gestion_usuario==1:
+                nombre=input("Ingrese nombre del usuario: ")
+                apellido=input("Ingrese apellido del usuario: ")
+                dni=input("Ingrese dni del usuario: ")
+                registro_usuarios=usuarios.registrar_usuario(nombre=nombre,apellido=apellido,dni=dni,registro_usuarios=registro_usuarios)
                    
-                   if gestion_usuario==1:
-                       nombre=input("Ingrese nombre del usuario: ")
-                       apellido=input("Ingrese apellido del usuario: ")
-                       dni=input("Ingrese dni del usuario: ")
-                       registro_usuarios=usuarios.registrar_usuario(nombre=nombre,apellido=apellido,dni=dni,registro_usuarios=registro_usuarios)
-                   
-                   elif gestion_usuario==2:
-                       dni=input("Ingrese dni del usuario: ")
-                       usuarios.eliminar_usuario(dni=dni,registro_usuarios=registro_usuarios)
+            elif gestion_usuario==2:
+                dni=input("Ingrese dni del usuario: ")
+                usuarios.eliminar_usuario(dni=dni,registro_usuarios=registro_usuarios)
                                    
-                   elif gestion_usuario==3:
-                        usuarios.listar_usuarios(inventario=inventario,registro_usuarios=registro_usuarios)
+            elif gestion_usuario==3:
+                usuarios.listar_usuarios(inventario=inventario,registro_usuarios=registro_usuarios)
 
-                   elif gestion_usuario==4:
-                       break
-               
-               except ValueError:
-                   print("Por favor seleccione el numero de la accion que desea realizar")
+            elif gestion_usuario==4:
+                break
             
-        elif gestion==3:
+    elif gestion==3:
             
-            gestion_prestamo=1
-            while gestion_prestamo > 0 and gestion_prestamo < 4:
-                try:
-                    gestion_prestamo=int(input(" 1. Prestar libro\n 2. Devolver libro\n 3. Volver al menu anterior\nQue desea hacer: "))
-                    if gestion_prestamo==1:
-                        libro=input("Indique el nombre del libro a prestar: ")
-                        dni=input("Indique DNI del usuario: ")
-                        prestamos.prestar_libro(dni=dni,libro=libro,registro_usuarios=registro_usuarios,inventario=inventario)
+        while True:
+            gestion_prestamo=menus.menu_prestamos()
+
+            if gestion_prestamo==1:
+                libro=input("Indique el nombre del libro a prestar: ")
+                dni=input("Indique DNI del usuario: ")
+                prestamos.prestar_libro(dni=dni,libro=libro,registro_usuarios=registro_usuarios,inventario=inventario)
                         
-                    elif gestion_prestamo==2:
-                        libro=input("Indique el nombre del libro a devolver: ")
-                        dni=input("Indique DNI del usuario: ")
-                        prestamos.devolver_libro(dni=dni,libro=libro,registro_usuarios=registro_usuarios,inventario=inventario)
+            elif gestion_prestamo==2:
+                libro=input("Indique el nombre del libro a devolver: ")
+                dni=input("Indique DNI del usuario: ")
+                prestamos.devolver_libro(dni=dni,libro=libro,registro_usuarios=registro_usuarios,inventario=inventario)
 
-                    elif gestion_prestamo==3:
-                        break
+            elif gestion_prestamo==3:
+                break
 
-                except ValueError:
-                    print("Por favor seleccione el numero de la accion que desea realizar")
-
-        elif gestion==4:
-            print("Hasta pronto!")
-            break
-
-    except ValueError:
-        print("Por favor seleccione el numero de la accion que desea realizar")
+    elif gestion==4:
+        print("Hasta pronto!")
+        break
 
 
 
--- menus.py
+++ menus.py
@@ -1,34 +1,42 @@
-def validar_opcion(opcion,opciones):
+def validar_opcion(menu,opciones):
     while True:
         try:
+            opcion=int(input(menu))  
             if opcion in opciones:
-               opcion
                return opcion
       
             print("Por favor seleccione el numero de la accion que desea realizar")
         except ValueError:
             print("Por favor seleccione el numero de la accion que desea realizar")
 
-def menu_libros():
+def menu_principal():
+   menu=" 1. Gestionar libros\n 2. Gestionar usuarios\n 3. Gestionar prestamos\n 4. Finalizar gestion\nQue desea hacer: "
+   gestion=validar_opcion(menu,{1,2,3,4})
+   return gestion  
 
-   opcion=int(input(" 1. Añadir libro\n 2. Eliminar libro\n 3. Buscar libros\n 4. Volver al menu anteriror\nQue desea hacer: "))
-   gestion_libro=validar_opcion(opcion,{1,2,3,4})
+def menu_libros():
+   menu=" 1. Añadir libro\n 2. Eliminar libro\n 3. Buscar libros\n 4. Volver al menu anteriror\nQue desea hacer: "
+   gestion_libro=validar_opcion(menu,{1,2,3,4})
    return gestion_libro
 
+def menu_filtros():
+   menu=" 1. Autor\n 2. Año de publicacion\n 3. Genero\n 4.Quitar filtro de autor\n 5. Quitar filtro de año\n 6.Quitar filtro de genero\n 7. Buscar\n 8. Volver\nDetermine filtro de la busqueda: "
+   gestion_filtro=validar_opcion(menu,{1,2,3,4,5,6,7,8})
+   return gestion_filtro
+
 def menu_busqueda():
-   opcion=int(input(" 1. Añadir usuario\n 2. Eliminar usuario\n 3. Buscar usuarios\n 4. Volver al menu anteriror\nQue desea hacer: "))
-   gestion_busqueda=validar_opcion(opcion,{1,2,3,4})
+   menu=" 1. Listar todos\n 2. Listar disponibles para prestar\n 3. Listar prestados\n 4. Buscar por filtro\n 5. Volver al menu anterior\nQue desea hacer:"
+   gestion_busqueda=validar_opcion(menu,{1,2,3,4,5})
    return gestion_busqueda
 
 def menu_usuarios():
-
-   opcion=int(input(" 1. Añadir usuario\n 2. Eliminar usuario\n 3. Buscar usuarios\n 4. Volver al menu anteriror\nQue desea hacer: "))
-   gestion_usuario=validar_opcion(opcion,{1,2,3,4})
+   menu=" 1. Añadir usuario\n 2. Eliminar usuario\n 3. Listar usuarios\n 4. Volver al menu anteriror\nQue desea hacer: "
+   gestion_usuario=validar_opcion(menu,{1,2,3,4})
    return gestion_usuario
 
 def menu_prestamos():
-   opcion=int(input(" 1. Añadir usuario\n 2. Eliminar usuario\n 3. Buscar usuarios\n 4. Volver al menu anteriror\nQue desea hacer: "))
-   gestion_prestamos=validar_opcion(opcion,{1,2,3,4})
+   menu=" 1. Prestar libro\n 2. Devolver libro\n 3. Volver al menu anterior\nQue desea hacer: "
+   gestion_prestamos=validar_opcion(menu,{1,2,3})
    return gestion_prestamos
 
 
